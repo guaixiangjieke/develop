0ba0614e13f1cb6887e79f3ad40ebba0f06ed874
ex-niulei002 <Dabowei1109>
2018-03-01 10:08:29 +0800

    1、fragment 包修改

--- develop/src/main/java/com/nl/develop/mvp/MvpPresenter.java
+++ develop/src/main/java/com/nl/develop/mvp/MvpPresenter.java
@@ -1,11 +1,11 @@
 package com.nl.develop.mvp;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 
 import com.nl.develop.DevelopConfig;
 import com.nl.develop.R;
