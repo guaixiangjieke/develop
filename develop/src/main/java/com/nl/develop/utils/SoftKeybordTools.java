d02bbb9282452b06196f114a4ad272efbd5397e3
ex-niulei002 <Dabowei1109>
2018-03-07 10:44:34 +0800

    1、软键盘工具类 打开关闭 搜索

---
+++ develop/src/main/java/com/nl/develop/utils/SoftKeybordTools.java
@@ -0,0 +1,86 @@
+package com.nl.develop.utils;
+
+import android.content.Context;
+import android.os.IBinder;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+
+/**
+ * Created by NiuLei on 2018/3/7.
+ * 软键盘工具
+ */
+
+public class SoftKeybordTools {
+    /**
+     * 打开软键盘
+     *
+     * @param context
+     * @param currentFocus
+     */
+    public static void showSoftInput(Context context, View currentFocus) {
+        if (context == null) {
+            return;
+        }
+
+        final InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null && !imm.isActive() && currentFocus != null) {
+            imm.showSoftInput(currentFocus, 0);
+        }
+    }
+
+    /**
+     * 隐藏软键盘
+     *
+     * @param context
+     * @param currentFocus
+     */
+    public static void hideSoftInput(Context context, View currentFocus) {
+        if (context == null) {
+            return;
+        }
+
+        final InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null && imm.isActive() && currentFocus != null) {
+            final IBinder windowToken = currentFocus.getWindowToken();
+            if (windowToken != null) {
+                imm.hideSoftInputFromWindow(windowToken, InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public static class ActionListener implements TextView.OnEditorActionListener {
+        private int actionId;
+        private Runnable task;
+
+        public ActionListener(int actionId, Runnable task) {
+            this.actionId = actionId;
+            this.task = task;
+        }
+
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            if (this.actionId == actionId) {
+                task.run();
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /**
+     * 软键盘搜索事件
+     */
+    public static class SearchActionListener extends ActionListener {
+
+        public SearchActionListener(Runnable task) {
+            super(EditorInfo.IME_ACTION_SEARCH, task);
+        }
+
+    }
+}
