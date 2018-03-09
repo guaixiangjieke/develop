3be4e54558d854b02a3147612619346c933e57ff
ex-niulei002 <Dabowei1109>
2018-03-09 16:30:32 +0800

    1、绘制视图树回调

--- develop/src/main/java/com/nl/develop/utils/ViewTools.java
+++ develop/src/main/java/com/nl/develop/utils/ViewTools.java
@@ -1,7 +1,10 @@
 package com.nl.develop.utils;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 /**
  * Created by NiuLei on 2018/3/8.
@@ -21,5 +24,22 @@ public class ViewTools {
             recyclerView.setHasFixedSize(true);
             recyclerView.setNestedScrollingEnabled(false);
         }
+    }
+
+    /**
+     * 绘制视图树时执行 任务
+     * @param view
+     * @param runnable
+     */
+    public static void onPreDrawOfView(@NonNull final View view, @NonNull final Runnable runnable) {
+        final ViewTreeObserver viewTreeObserver = view.getViewTreeObserver();
+        viewTreeObserver.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                view.getViewTreeObserver().removeOnPreDrawListener(this);
+                runnable.run();
+                return false;
+            }
+        });
     }
 }
